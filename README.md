ca38d583c0c909634624fd417764b790b7f811ea
Sergey Petrinich <petrinichshadow@gmail.com>
2022-10-18 20:43:36 +0300

    firebase deploy

---
+++ README.md
@@ -0,0 +1,4 @@
+## Links
+
+<a href="https://pet-realestate.web.app">Home Page [1 link]</a> \
+<a href="https://pet-realestate.firebaseapp.com">Home Page [2 link]</a>
--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -1,33 +1,11 @@
-/*
-Документация по работе в шаблоне: 
-Документация слайдера: https://swiperjs.com/
-Сниппет(HTML): swiper
-*/
-
-// Подключаем слайдер Swiper из node_modules
-// При необходимости подключаем дополнительные модули слайдера, указывая их в {} через запятую
-// Пример: { Navigation, Autoplay }
 import Swiper, { Pagination, EffectFade } from "swiper";
-/*
-Основниые модули слайдера:
-Navigation, Pagination, Autoplay, 
-EffectFade, Lazy, Manipulation
-Подробнее смотри https://swiperjs.com/
-*/
 
 import "../../scss/base/swiper.scss";
 
 // Инициализация слайдеров
 function initSliders() {
-    // Перечень слайдеров
-    // Проверяем, есть ли слайдер на стронице
     if (document.querySelector(".mainslider")) {
-        // Указываем скласс нужного слайдера
-        // Создаем слайдер
         new Swiper(".mainslider", {
-            // Указываем скласс нужного слайдера
-            // Подключаем модули слайдера
-            // для конкретного случая
             modules: [Pagination, EffectFade],
             observer: true,
             observeParents: true,
@@ -36,11 +14,8 @@ function initSliders() {
             grabCursor: true,
             autoHeight: true,
             speed: 800,
-            // Эффекты
             effect: "fade",
             fadeEffect: {
-                // Параллельная
-                // смена прозрачности
                 crossFade: true,
             },
 
@@ -48,8 +23,6 @@ function initSliders() {
                 el: ".mainslider-dotts",
                 clickable: true,
             },
-
-            on: {},
         });
         let mainsliderImages = document.querySelectorAll(".mainslider__slide-ibg");
         let mainsliderDotts = document.querySelectorAll(".mainslider-dotts .swiper-pagination-bullet");
@@ -60,12 +33,7 @@ function initSliders() {
     }
 
     if (document.querySelector(".similar-content__items")) {
-        // Указываем скласс нужного слайдера
-        // Создаем слайдер
         new Swiper(".similar-content__items", {
-            // Указываем скласс нужного слайдера
-            // Подключаем модули слайдера
-            // для конкретного случая
             observer: true,
             observeParents: true,
             slidesPerView: 3,
@@ -86,8 +54,6 @@ function initSliders() {
                     slidesPerView: 3,
                 },
             },
-
-            on: {},
         });
     }
 }
